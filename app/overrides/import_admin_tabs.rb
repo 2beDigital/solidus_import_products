1cafe6cbc3e1c4047d64387debfb7786df198ae9
Micah Roberson <micah.roberson@gmail.com>
2012-09-20 19:06:46 -0700

    Updating admin tab deface

--- app/overrides/import_admin_tabs.rb
+++ app/overrides/import_admin_tabs.rb
@@ -1,5 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "user_admin_tabs",
+                     :name => "import_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:product_imports) %>",
-                     :disabled => false)
+                     :text => "<%= tab(:product_imports) %>")
