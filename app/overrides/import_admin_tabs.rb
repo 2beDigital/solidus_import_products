9ff8f9f0539335683bffbf2dd8b631dd07d189c8
Micah Roberson <micah.roberson@gmail.com>
2012-09-20 19:00:04 -0700

    promo_admin_tabs changed to user_admin_tabs

--- app/overrides/import_admin_tabs.rb
+++ app/overrides/import_admin_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "promo_admin_tabs",
+                     :name => "user_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text => "<%= tab(:product_imports) %>",
                      :disabled => false)
