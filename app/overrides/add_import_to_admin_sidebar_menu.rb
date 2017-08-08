ba874a917be6e2470566351623e2b4a894e79b2b
Noel <noel@2bedigital.com>
2017-08-08 08:29:47 +0100

    fixes in override, insert access from sidebar settings menu, SOL-61

--- app/overrides/add_import_to_admin_sidebar_menu.rb
+++ app/overrides/add_import_to_admin_sidebar_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
-    virtual_path: "spree/admin/shared/_product_sub_menu",
+    virtual_path: "spree/admin/shared/_settings_sub_menu",
     name: "product_import_admin_sidebar_menu",
-    insert_bottom: "[data-hook='admin_product_sub_tabs']",
+    insert_bottom: "[data-hook='admin_settings_sub_tabs']",
     partial: 'spree/admin/shared/import_sidebar_menu'
 )
