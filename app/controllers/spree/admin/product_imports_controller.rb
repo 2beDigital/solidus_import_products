99b21625f8b2347554cf7dbda01a23d6a1a1e746
Noel <noel@2bedigital.com>
2019-03-20 13:11:13 +0000

    added upload multiple files

--- app/controllers/spree/admin/product_imports_controller.rb
+++ app/controllers/spree/admin/product_imports_controller.rb
@@ -12,18 +12,40 @@ module Spree
       end
 
       def create
-        @product_import = Spree::ProductImport.create(product_import_params)
-				@product_import.created_by=spree_current_user.id
-				@product_import.save
+        import = product_import_params.to_h
+        import.merge(created_by: spree_current_user.id)
+        data_files = import.delete("data_file")
+        delayed_imports = false
+        if data_files.size > 1
+          data_files.each do |data_file|
+            import["data_file"] = data_file
+            @product_import = Spree::ProductImport.create(import)
+            if @product_import.productsCount > Spree::ProductImport.settings[:num_prods_for_delayed]
+              ImportProductsJob.perform_later(@product_import.id, current_store.id)
+              delayed_imports = true
+            else
+              @product_import.import_data!(Spree::ProductImport.settings[:transaction])
+            end
+          end
+          if delayed_imports
+            flash[:notice] = t('product_import_processing')
+          else
+            flash[:success] = t('product_import_imported')
+          end
+          redirect_to admin_product_imports_path
+          return
+        end
+        import["data_file"] = data_files[0]
+        @product_import = Spree::ProductImport.create(import)
         begin
-          numProds=@product_import.productsCount
-          if numProds > Spree::ProductImport.settings[:num_prods_for_delayed]
+          delayed_imports = (@product_import.productsCount > Spree::ProductImport.settings[:num_prods_for_delayed]) ? true : false
+          if delayed_imports
 						ImportProductsJob.perform_later(@product_import.id)
 					  flash[:notice] = t('product_import_processing')
           else
             @product_import.import_data!(Spree::ProductImport.settings[:transaction])
 					  flash[:success] = t('product_import_imported')
-            end
+          end
         rescue StandardError => e
           @product_import.error_message=e.message
           @product_import.failure
