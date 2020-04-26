5dbad9cdb309f2577284d1f0b0c4119e02df3fac
Memo Toro <memo.toro@gmail.com>
2020-04-26 08:44:17 +0100

    Solidus Support updated 'solidus_support', '~> 0.5'

--- app/decorators/core/spree/permission_sets/product_management_decorator.rb
+++ app/decorators/core/spree/permission_sets/product_management_decorator.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
-Spree::PermissionSets::ProductManagement.class_eval do
+module Spree
+  module PermissionSets
+    module ProductManagementDecorator
+      # Self prepend to extend feature at the class level
+      def self.prepended(base)
+        base.class_eval do
+          # Create an alias_method for the original activate! method as we need to execute it as it is
+          alias_method :super_activate!, :activate!
+        end
+      end
 
-  # Create an alias_method for the original activate! method as we need to execute it as it is
-  alias_method :super_activate!, :activate!
+      # Extends the behaviour of the activate! method by calling the original method and adding an extra permission set for Spree::ProductImport
+      def activate!
+        super_activate!
+        can :manage, Spree::ProductImport
+      end
 
-  # Extends the behaviour of the activate! method by calling the original method and adding an extra permission set for Spree::ProductImport
-  def activate!
-    super_activate!
-    can :manage, Spree::ProductImport
+      ::Spree::PermissionSets::ProductManagement.prepend self
+    end
   end
-
 end
