0c05f2ad3dd61e1ccf27747308de5e06c1f9a7e7
Memo Toro <memo.toro@gmail.com>
2020-04-26 10:49:01 +0100

    Extension class_eval approach

--- app/decorators/core/spree/permission_sets/product_management_decorator.rb
+++ app/decorators/core/spree/permission_sets/product_management_decorator.rb
@@ -1,23 +1,12 @@
 # frozen_string_literal: true
 
-module Spree
-  module PermissionSets
-    module ProductManagementDecorator
-      # Self prepend to extend feature at the class level
-      def self.prepended(base)
-        base.class_eval do
-          # Create an alias_method for the original activate! method as we need to execute it as it is
-          alias_method :super_activate!, :activate!
-        end
-      end
+Spree::PermissionSets::ProductManagement.class_eval do
+  # Create an alias_method for the original activate! method as we need to execute it as it is
+  alias_method :super_activate!, :activate!
 
-      # Extends the behaviour of the activate! method by calling the original method and adding an extra permission set for Spree::ProductImport
-      def activate!
-        super_activate!
-        can :manage, Spree::ProductImport
-      end
-
-      ::Spree::PermissionSets::ProductManagement.prepend self
-    end
+  # Extends the behaviour of the activate! method by calling the original method and adding an extra permission set for Spree::ProductImport
+  def activate!
+    super_activate!
+    can :manage, Spree::ProductImport
   end
 end
